66d4468237999586c47f865d50093cdd3fa06e82
Shubham Bisht <bistshubham2016@gmail.com>
2024-05-01 11:34:20 +0530

    shadcn and dark theme init

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,12 +1,11 @@
+import { Separator } from "@/components/ui/separator";
+import { SignedIn } from "@clerk/clerk-react";
+import { UserButton } from "@clerk/nextjs";
+
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <header></header>
-      <main>{children}</main>
-    </>
-  );
+  return <></>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { ToggleTheme } from "@/components/ToggleTheme";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,8 +19,38 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className="scroll-smooth antialiased">
-        <body className={inter.className}>{children}</body>
+      <html
+        lang="en"
+        className="scroll-smooth antialiased"
+        suppressHydrationWarning
+      >
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-h-screen w-full flex flex-col items-center dark:bg-black">
+              <header className="flex justify-between items-center h-[60px] p-4 px-8 w-full">
+                {/* logo */}
+                <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent">
+                  RemindMe
+                </h1>
+                <div className="flex items-center gap-4">
+                  <SignedIn>
+                    <UserButton />
+                  </SignedIn>
+                  <ToggleTheme />
+                </div>
+              </header>
+              <Separator />
+              <main className="flex justify-center items-center w-full grow">
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
